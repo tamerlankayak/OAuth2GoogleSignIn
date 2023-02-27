dffd8907ee88669426ca8991c40b27df8d4fcc73
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-02-27 20:17:22 +0400

    Update HelloWorldController.java

--- src/main/java/com/example/oauthgooglesignin/HelloWorldController.java
+++ src/main/java/com/example/oauthgooglesignin/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//sign in with google auth and get some responses from api
 @RestController
 public class HelloWorldController {
 
